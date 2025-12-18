7145e058186ab7898f8a2abcba9436f2447d100c
afsaanchez04 <afsanchez04@gmail.com>
2025-12-17 21:01:10 -0500

    filesystem síncrono y asíncrono

--- 07-divide-and-conquer/proyecto/app.js
+++ 07-divide-and-conquer/proyecto/app.js
@@ -2,21 +2,21 @@
 
 function findMax(arr) {
     // TODO: Agregar la condición del caso base
-    if (/* condición */) {
-        return /* valor */;
-    }
+    //if (/* condición */) {
+      //  return /* valor */;
+    //}
 
     // TODO: Dividir el arreglo en dos mitades
-    const mid = /* calcular el punto medio */;
-    const left = /* obtener la primera mitad */;
-    const right = /* obtener la segunda mitad */;
+    // const mid = /* calcular el punto medio */
+    // const left = /* obtener la primera mitad */
+    // const right = /* obtener la segunda mitad */
 
     // TODO: Llamar recursivamente a la función para ambas mitades
-    const leftMax = /* llamada recursiva */;
-    const rightMax = /* llamada recursiva */;
+    // const leftMax = /* llamada recursiva */
+    // const rightMax = /* llamada recursiva */
 
     // TODO: Combinar las soluciones comparando los máximos
-    return /* máximo entre leftMax y rightMax */;
+    //return /* máximo entre leftMax y rightMax */;
 }
 // Ejemplo de entrada
 const numbers = [3, 8, 2, 10, 5, 7];
---
+++ 08-manejo-archivos-node/app.js
@@ -0,0 +1,119 @@
+
+const fs = require("fs")
+
+//Leer archivo (Asíncrono)
+//1. Ruta del archivo
+//2. Set de caracteres UTF-8
+//3. Función se va a ejecutar después del tiempo que tome la función en resolverse
+/* fs.readFile("index.html","UTF-8", (err, contenido) => {
+  if(err){
+    console.log(err)
+  }else{
+    console.log(contenido)
+  }
+}) */
+
+//Renombrar un archivo
+//1. Ruta del archivo
+//2. Nuevo nombre del archivo
+//3. Función se va a ejecutar después del tiempo que tome la función en resolverse
+/* fs.rename("index.html","main.html", (err) => {
+  if(err){
+    throw err
+  }
+
+  console.log("Nombre modificado")
+
+}) */
+
+//Agregar contenido al final del archivo
+//1. Ruta del archivo
+//2. Nuevo contenido
+//3. Función se va a ejecutar después del tiempo que tome la función en resolverse
+/* fs.appendFile( "main.html", "<p>Hola Node</p>", (err) => {
+  if(err){
+    throw err
+  }
+
+  console.log("Contenido modificado")
+
+} ) */
+
+//Cuando queremos reemplazar todo el contenido del archivo
+//1. Ruta del archivo
+//2. Nuevo contenido
+//3. Función se va a ejecutar después del tiempo que tome la función en resolverse
+/* fs.writeFile( "main.html", "Contenido nuevo", (err) => {
+  if(err){
+    throw err
+  }
+
+  console.log("Información reemplazada")
+} ) */
+
+
+//Eliminar archivo
+//1. Ruta del archivo
+//2. Función se va a ejecutar después del tiempo que tome la función en resolverse
+
+/* fs.unlink( "main.html", (err) => {
+  if(err){
+    throw err
+  }
+
+  console.log("Archivo eliminado")
+} ) */
+
+
+
+//Prueba asincronía 
+
+/* console.log("1. Antes de leer el archivo")
+
+fs.readFile("index.html", "UTF-8", (err, contenido) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(contenido)
+  }
+})
+
+console.log("2. Después de leer el archivo")
+
+fs.rename("index.html","main.html", (err) => {
+  if(err){
+    throw err
+  }
+
+  console.log("Nombre modificado")
+
+})
+
+console.log("3. Después de renombrar el archivo")
+
+console.log("4. Después de añadir el contenido") */
+
+
+
+
+//utilizando las versiones síncronas de los métodos de file system
+
+/* console.log("Antes de leer el archivo")
+
+const archivo = fs.readFileSync("main.html", "utf-8")
+console.log(archivo)
+
+console.log("Después de leer el archivo")
+
+fs.renameSync("main.html","index.html")
+
+console.log("Después de renombrar el archivo")
+
+fs.appendFileSync("index.html","<p>Hola de nuevo</p>")
+
+console.log("Después añadir información al archivo") */
+
+
+
+//
+
